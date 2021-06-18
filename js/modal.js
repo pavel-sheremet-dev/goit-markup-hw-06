36fae086b6ca3424680851d528f43c5e9c349f90
Pavel Sheremet <pashqa.sheremet@gmail.com>
2021-06-18 08:26:05 +0300

    prepare to hw7

--- js/modal.js
+++ js/modal.js
@@ -3,14 +3,12 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    modalWindow: document.querySelector('[data-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-backdrop');
-    refs.modalWindow.classList.toggle('is-hidden-modal');
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
